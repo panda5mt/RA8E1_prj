cff3fad90de5a69c7521f1867822403bb944e765
Atsushi Saitoh <lynxeyed@live.jp>
2024-12-17 12:47:25 +0900

    remove cache

--- src/blinky_thread_entry.c
+++ src/blinky_thread_entry.c
@@ -58,6 +58,6 @@ void blinky_thread_entry(void *pvParameters)
             pin_level = BSP_IO_LEVEL_LOW;
         }
 
-        vTaskDelay(pdMS_TO_TICKS(125));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
