8cf53467156ffe2f80062d9ba696c4f11a197793
Atsushi Saitoh <lynxeyed@live.jp>
2024-12-27 11:14:57 +0900

    Update main_thread0_entry.c

--- src/main_thread0_entry.c
+++ src/main_thread0_entry.c
@@ -4,11 +4,28 @@
 #include "xprintf_helper.h"
 #include "sccb_if.h"
 
+bool g_ceu_capture_error;
+bool g_ceu_capture_complete;
+
 void g_ceu0_user_callback(capture_callback_args_t *p_args)
 {
-    if (CEU_EVENT_FRAME_END == p_args->event)
+    /* Multiple event flags may be set simultaneously */
+    if (p_args->event & (uint32_t) ~(CEU_EVENT_HD | CEU_EVENT_VD | CEU_EVENT_FRAME_END))
     {
-        /// g_capture_ready = true;
+        /* Error processing should occur first. Application should not process complete event if error occurred. */
+        g_ceu_capture_error = true;
+    }
+    else
+    {
+        if (p_args->event & CEU_EVENT_VD)
+        {
+            /* Capture has started. Process V-Sync event. */
+        }
+        if (p_args->event & CEU_EVENT_FRAME_END)
+        {
+            /* Capture is complete and no error has occurred */
+            g_ceu_capture_complete = true;
+        }
     }
 }
 
@@ -25,6 +42,7 @@ void main_thread0_entry(void *pvParameters)
     sccb_and_clk_init();
 
     R_CEU_Open(&g_ceu0_ctrl, &g_ceu0_cfg);
+
     /* TODO: add your own code here */
     while (1)
     {
