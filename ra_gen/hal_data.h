015cc5fb7023083f2533d64e12cffd148a6f4075
Atsushi Saitoh <lynxeyed@live.jp>
2025-01-02 16:30:50 +0900

    increase I2C speed.

--- ra_gen/hal_data.h
+++ ra_gen/hal_data.h
@@ -7,9 +7,9 @@
 #include "r_iic_master.h"
 #include "r_i2c_master_api.h"
 #include "r_capture_api.h"
-            #include "r_ceu.h"
+#include "r_ceu.h"
 #include "r_sci_b_uart.h"
-            #include "r_uart_api.h"
+#include "r_uart_api.h"
 #include "r_gpt.h"
 #include "r_timer_api.h"
 FSP_HEADER
@@ -21,27 +21,27 @@ extern iic_master_instance_ctrl_t g_i2c_master1_ctrl;
 extern const i2c_master_cfg_t g_i2c_master1_cfg;
 
 #ifndef g_i2c_callback
-void g_i2c_callback(i2c_master_callback_args_t * p_args);
+void g_i2c_callback(i2c_master_callback_args_t *p_args);
 #endif
 /* CEU on CAPTURE instance */
-            extern const capture_instance_t g_ceu0;
-            /* Access the CEU instance using these structures when calling API functions directly (::p_api is not used). */
-            extern ceu_instance_ctrl_t g_ceu0_ctrl;
-            extern const capture_cfg_t g_ceu0_cfg;
-            #ifndef g_ceu0_user_callback
-            void g_ceu0_user_callback(capture_callback_args_t * p_args);
-            #endif
+extern const capture_instance_t g_ceu0;
+/* Access the CEU instance using these structures when calling API functions directly (::p_api is not used). */
+extern ceu_instance_ctrl_t g_ceu0_ctrl;
+extern const capture_cfg_t g_ceu0_cfg;
+#ifndef g_ceu0_user_callback
+void g_ceu0_user_callback(capture_callback_args_t *p_args);
+#endif
 /** UART on SCI Instance. */
-            extern const uart_instance_t      g_uart9;
+extern const uart_instance_t g_uart9;
 
-            /** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
-            extern sci_b_uart_instance_ctrl_t     g_uart9_ctrl;
-            extern const uart_cfg_t g_uart9_cfg;
-            extern const sci_b_uart_extended_cfg_t g_uart9_cfg_extend;
+/** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_b_uart_instance_ctrl_t g_uart9_ctrl;
+extern const uart_cfg_t g_uart9_cfg;
+extern const sci_b_uart_extended_cfg_t g_uart9_cfg_extend;
 
-            #ifndef NULL
-            void NULL(uart_callback_args_t * p_args);
-            #endif
+#ifndef NULL
+void NULL(uart_callback_args_t *p_args);
+#endif
 /** Timer on GPT Instance. */
 extern const timer_instance_t g_timer3;
 
@@ -50,7 +50,7 @@ extern gpt_instance_ctrl_t g_timer3_ctrl;
 extern const timer_cfg_t g_timer3_cfg;
 
 #ifndef NULL
-void NULL(timer_callback_args_t * p_args);
+void NULL(timer_callback_args_t *p_args);
 #endif
 void hal_entry(void);
 void g_hal_init(void);
--- ra_gen/main_thread1.h
+++ ra_gen/main_thread1.h
@@ -2,15 +2,15 @@
 #ifndef MAIN_THREAD1_H_
 #define MAIN_THREAD1_H_
 #include "bsp_api.h"
-                #include "FreeRTOS.h"
-                #include "task.h"
-                #include "semphr.h"
-                #include "hal_data.h"
-                #ifdef __cplusplus
-                extern "C" void main_thread1_entry(void * pvParameters);
-                #else
-                extern void main_thread1_entry(void * pvParameters);
-                #endif
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "hal_data.h"
+#ifdef __cplusplus
+extern "C" void main_thread1_entry(void *pvParameters);
+#else
+extern void main_thread1_entry(void *pvParameters);
+#endif
 FSP_HEADER
 FSP_FOOTER
 #endif /* MAIN_THREAD1_H_ */
