ae62abe8ba45190ee87608dfc169e45ed4c93ad5
Atsushi Saitoh <lynxeyed@live.jp>
2025-05-13 02:06:56 +0900

    p1

--- src/xprintf/src/xprintf_helper.c
+++ src/xprintf/src/xprintf_helper.c
@@ -18,15 +18,15 @@ void put_char_ra8(uint8_t ch)
     {
         R_BSP_MODULE_START(FSP_IP_SCI, 9);
         // init UART & printf
-        R_SCI_B_UART_Open(&g_uart9_ctrl, &g_uart9_cfg);
+        // R_SCI_B_UART_Open(&g_uart9_ctrl, &g_uart9_cfg);
         first_call = false;
     }
 
     uint32_t len = (uint32_t)(sizeof(p) / sizeof(uint8_t));
 
-    R_SCI_B_UART_Write(&g_uart9_ctrl,
-                       p,
-                       (len == 1) ? len += 1 : len);
+    // R_SCI_B_UART_Write(&g_uart9_ctrl,
+    //                    p,
+    //                    (len == 1) ? len += 1 : len);
 }
 
 // void put_char_ra8_usb(uint8_t ch)
