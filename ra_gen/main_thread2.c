2eb60f16630c9a46d86c63e5ad49d18e0f5e18e3
Atsushi Saitoh <lynxeyed@live.jp>
2025-05-15 17:43:15 +0900

    update

--- ra_gen/main_thread2.c
+++ ra_gen/main_thread2.c
@@ -4,9 +4,9 @@
 #if 1
                 static StaticTask_t main_thread2_memory;
                 #if defined(__ARMCC_VERSION)           /* AC6 compiler */
-                static uint8_t main_thread2_stack[4096] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.thread") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
+                static uint8_t main_thread2_stack[1024] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.thread") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
                 #else
-                static uint8_t main_thread2_stack[4096] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.main_thread2") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
+                static uint8_t main_thread2_stack[1024] BSP_PLACE_IN_SECTION(BSP_UNINIT_SECTION_PREFIX ".stack.main_thread2") BSP_ALIGN_VARIABLE(BSP_STACK_ALIGNMENT);
                 #endif
                 #endif
                 TaskHandle_t main_thread2;
@@ -126,7 +126,7 @@ extern uint32_t g_fsp_common_thread_count;
                     #endif
                         main_thread2_func,
                         (const char *)"Main Thread2",
-                        4096/4, // In words, not bytes
+                        1024/4, // In words, not bytes
                         (void *) &main_thread2_parameters, //pvParameters
                         1,
                         #if 1
