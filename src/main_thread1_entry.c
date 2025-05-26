6151e8908a0c6415dc836ce687b12e2504cff0db
Atsushi Saitoh <lynxeyed@live.jp>
2025-05-26 21:09:05 +0900

    Update main_thread1_entry.c

--- src/main_thread1_entry.c
+++ src/main_thread1_entry.c
@@ -11,10 +11,10 @@
 #define ETHER_EXAMPLE_DESTINATION_MAC_ADDRESS 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
 #define ETHER_EXAMPLE_FRAME_TYPE 0x00, 0x2E
 #define ETHER_EXAMPLE_PAYLOAD 'I', '\'', 'm', ' ', 'R', 'A', '8', 'E', '1', '.', \
-                              'U', 'N', 'K', 'O', '!', 'U', 'N', 'K', 'O', '!',  \
-                              'U', 'N', 'K', 'O', '!', 'U', 'N', 'K', 'O', '!',  \
-                              'U', 'N', 'K', 'O', '!', 'U', 'N', 'K', 'O', '!',  \
-                              'U', 'N', 'K', 'O', '!'
+                              ' ', 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r',  \
+                              'l', 'd', '!', '!', ' ', 'H', 'E', 'L', 'L', 'O',  \
+                              ' ', 'W', 'O', 'R', 'L', 'D', '!', ' ', 'R', 'e',  \
+                              'n', 'e', 's', 'a', 's'
 
 #define ETHER_EXAMPLE_FLAG_ON (1U)
 #define ETHER_EXAMPLE_FLAG_OFF (0U)
@@ -27,12 +27,12 @@ static volatile uint32_t g_example_link_on = 0;
 #define PHY_BCR_RESET (1 << 15)
 #define PHY_BCR_AUTONEGO_EN (1 << 12)
 #define PHY_BCR_RESTART_AUTONEGO (1 << 9)
-
 __attribute__((aligned(32))) uint8_t gp_send_data_internal[ETHER_EXAMPLE_TRANSMIT_ETHERNET_FRAME_SIZE] = {
     ETHER_EXAMPLE_DESTINATION_MAC_ADDRESS, /* Destination MAC address */
     ETHER_EXAMPLE_SOURCE_MAC_ADDRESS,      /* Source MAC address */
     ETHER_EXAMPLE_FRAME_TYPE,              /* Type field */
     ETHER_EXAMPLE_PAYLOAD                  /* Payload value (46byte) */
+    // after bytes, filled with zeros
 };
 
 void ether_example_callback(ether_callback_args_t *p_args)
@@ -60,8 +60,6 @@ void ether_example_callback(ether_callback_args_t *p_args)
 
     default:
         xprintf("[ISR] Event: %d\n", p_args->event);
-        g_example_link_on = 0;
-        g_example_transfer_complete = 0;
         break;
     }
 }
@@ -91,6 +89,7 @@ void main_thread1_entry(void *pvParameters)
 
     // Check Link ON
     g_example_link_on = 0;
+
     do
     {
         err = R_ETHER_LinkProcess(&g_ether0_ctrl);
@@ -103,30 +102,28 @@ void main_thread1_entry(void *pvParameters)
 
     xprintf("LINK ON\n");
 
-    for (int i = 0; i < 10; i++)
+    g_example_transfer_complete = 0;
+    /* Set user buffer to TX descriptor and enable transmission. */
+    err = R_ETHER_Write(&g_ether0_ctrl, (void *)gp_send_data_internal, sizeof(gp_send_data_internal));
+    if (FSP_SUCCESS == err)
     {
-
-        g_example_transfer_complete = 0;
-        /* Set user buffer to TX descriptor and enable transmission. */
-        err = R_ETHER_Write(&g_ether0_ctrl, (void *)gp_send_data_internal, sizeof(gp_send_data_internal));
-        if (FSP_SUCCESS == err)
+        /* Wait for the transmission to complete. */
+        /* Data array should not change in zero copy mode until transfer complete. */
+        while (ETHER_EXAMPLE_FLAG_ON != g_example_transfer_complete)
         {
-            /* Wait for the transmission to complete. */
-            /* Data array should not change in zero copy mode until transfer complete. */
-            while (ETHER_EXAMPLE_FLAG_ON != g_example_transfer_complete)
-            {
-                ;
-            }
+            ;
         }
-        xprintf("[ETH]OK!\n");
     }
+    xprintf("[ETH]Write OK!\n");
 
     /* Get receive buffer from RX descriptor. */
     static uint8_t *p_read_buffer_nocopy;
     uint32_t read_data_size = 0;
     g_example_receive_complete = 0;
     err = R_ETHER_Read(&g_ether0_ctrl, (void *)&p_read_buffer_nocopy, &read_data_size);
+    xprintf("[ETH] RCV result:%d\n", err);
     assert(FSP_SUCCESS == err);
+
     /* Process received data here */
     if (FSP_SUCCESS == err)
     {
@@ -138,14 +135,14 @@ void main_thread1_entry(void *pvParameters)
         }
     }
     vTaskDelay(pdMS_TO_TICKS(1));
-
+    xprintf("[ETH]RCV OK!\n");
     /* Release receive buffer to RX descriptor. */
     err = R_ETHER_BufferRelease(&g_ether0_ctrl);
     assert(FSP_SUCCESS == err);
-    xprintf("[ETH]RCV OK!\n");
+
     // /* Disable transmission and receive function and close the ether instance. */
     R_ETHER_Close(&g_ether0_ctrl);
-
+    xprintf("[ETH]Close.\n");
     while (1)
     {
         vTaskDelay(pdMS_TO_TICKS(1000));
